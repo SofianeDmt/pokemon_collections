13cfdbc0c083f5b4725d9cbdbe3ca6621c876c76
Sofiane Demette <Sofiane.dmt@hotmail.fr>
2023-04-16 15:36:43 +0200

    feat(front): add Navbar components to _document.tsx and change language

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Navbar from "@/components/navbar";
 
-export default function Document() {
+export default function Document(): JSX.Element {
   return (
-    <Html lang="en">
-      <Head />
+    <Html className="min-h-screen bg-gray-200" lang="fr">
+      <Head>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+      </Head>
       <body>
+      <Navbar/>
         <Main />
         <NextScript />
       </body>
